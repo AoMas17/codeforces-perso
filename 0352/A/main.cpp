36aaacea0e29a0171ff9433a8600a248f00cad2a
Alexandre <alexandresantacreu31@gmail.com>
2024-11-06 00:27:33 +0100

    oui

---
+++ 0352/A/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+#define init ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define FORi(a) for (int i = 0; i < a; i++)
+#define FORj(a) for (int j = 0; j < a; j++)
+#define FORk(a) for (int k = 0; k < a; k++)
+#define ull unsigned long long
+#define ll long long
+
+void Solve(){
+    cout << "HELLO WORLD!" << endl;	
+}
+	
+int main(){ 
+    init; 
+    Solve();
+    return 0;
+}
