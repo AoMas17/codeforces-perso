e7da561be6fac8de253d17b21f19d9d256c6cd03
Alexandre <alexandresantacreu31@gmail.com>
2024-11-07 00:29:06 +0100

    oui

---
+++ 0124/A/accepted.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+#define init ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define FORi(a) for (int i = 0; i < a; i++)
+#define FORj(a) for (int j = 0; j < a; j++)
+#define FORk(a) for (int k = 0; k < a; k++)
+#define ull unsigned long long
+#define ll long long
+
+void Solve(){
+    int n, a, b;
+    cin >> n >> a >> b;
+    int res = n - a;
+    res = min(res, b + 1);
+    cout << res << endl;
+}
+	
+int main(){ 
+    init; 
+    Solve();
+    return 0;
+}
